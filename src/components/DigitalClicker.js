661a90b64e2ddfc9bac776a0875bcb2e6352d125
shomiyata <eighteighttwenty@gmail.com>
2017-08-29 16:45:27 -0400

    Done.

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,19 @@
 // Code DigitalClicker Component Here
+import React from 'react'
+
+export default class DigitalClicker extends React.Component{
+
+	state = {
+		timesClicked: 0
+	}
+
+	addClick = () => {
+		this.setState({
+		timesClicked: ++this.state.timesClicked
+		})
+	}
+
+	render(){
+		return <button onClick={this.addClick}>{this.state.timesClicked}</button>
+	}
+}
--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,46 @@
 // Code YouTubeDebugger Component Here
+import React from 'react'
+
+export default class YouTubeDebugger extends React.Component {
+
+	state = {
+			errors: [], 
+			user: null, 
+			settings:{
+				bitrate: 8,
+					video:{
+						resolution: '1080p'
+					} 
+				}
+			}
+
+	handleClickBitrate = () => {
+		this.setState({
+			settings:{
+				bitrate: 12,
+				video:{
+					...this.state.settings.video,
+				}
+			}
+		})
+	}
+
+	handleClickRes = () =>{
+		this.setState({
+			settings:{
+				...this.state.settings,
+				video:{
+					resolution: '720p'
+				}
+			}
+		})
+	}
+
+	render(){
+		return (<div>
+			<button className='bitrate' onClick={this.handleClickBitrate}></button>
+			<button className='resolution' onClick={this.handleClickRes}></button>
+			</div>
+		)
+	}
+}
